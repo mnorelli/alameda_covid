3805cd767ce461e4df9c4ef128851a82a4afd42a
Mei Eisenbach <mei.eisenbach@gmail.com>
2020-04-21 22:17:00 -0700

    Added print statements to show progress

--- covid.py
+++ covid.py
@@ -13,10 +13,8 @@ def geo_code(df):
     df_new = df.copy()
     address = df.Address + ', ' + df.City + ', ' + df.State + ' ' + df.Zip.map(str)
     geo = geocode(address, provider='nominatim')
-    long = geo.geometry.x
-    lat = geo.geometry.y
-    df_new['Longitude'] = long
-    df_new['Latitude'] = lat
+    df_new['Longitude'] = geo.geometry.x
+    df_new['Latitude'] = geo.geometry.y
     return df_new
 
 if __name__ == "__main__":
@@ -29,18 +27,23 @@ if __name__ == "__main__":
     output = "output.txt"
     
     df_yesterday = pd.read_csv(yesterday, sep='\t')
+    print('{} record(s) read from {}'.format(df_yesterday.shape[0], yesterday))
+    
     df_today = pd.read_csv(today, sep='\t')
+    print('{} record(s) read from {}'.format(df_today.shape[0], today))
 
     df_yesterday.set_index('IncidentID', inplace=True)
     df_today.set_index('IncidentID', inplace=True)
 
     # new rows are indices in today's which are not in yesterday's
-    new_rows = ~df_today.index.isin(df_yesterday.index)    
+    new_rows = ~df_today.index.isin(df_yesterday.index)
+    print('{} new record(s) found'.format(df_today[new_rows].shape[0]))    
+
+    print('Geocoding new records...')    
     df_new = geo_code(df_today[new_rows])
 
     # merge new rows with yesterday's    
     df_out = pd.concat([df_yesterday, df_new])    
     df_out.to_csv(output, sep='\t')
     
-    print('{} new row(s) found'.format(df_new.shape[0]))    
-    print('Done')    
+    print('Merged data written to {}'.format(output))
